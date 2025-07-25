c35a2b72ddc975f66389ea518e4eb2aaf877e4da
Edna-dev <wanguieddie99@gmail.com>
2025-07-25 08:22:19 +0300

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -9,4 +9,11 @@ shopping_list = []
 
 display_menu()
 
-choice = int(input("Enter your choice: "))
+choice = input("Enter your choice (number): ")
+
+# Ensure the input is treated as a number
+try:
+    choice = int(choice)
+except ValueError:
+    print("Invalid input. Please enter a number.")
+
